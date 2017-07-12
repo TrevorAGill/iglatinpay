af6ea68ccf00d7c33d687d5f04d8f72bf3d58100
Kimberly Lu and Trevor Gill <kimberly@>
2017-07-12 16:04:55 -0700

    improve branching logic to prevent double evaluation of a word, and include branching to return results for non-words.

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,23 @@ $(document).ready(function() {
 
     var input = $("#sentenceinput").val();
     var inputArray = input.split("");
+    var char1 = inputArray[0];
     var firstVowelPosition = 0;
     var inputLength = input.length;
     firstChar = input.slice(0, 1);
 
+
+    if( char1 === "a" || char1==="e" || char1==="i" || char1==="o" || char1==="u"){
+      for (var i = 0 ; i < vowels.length; i++) {
+        if(char1 === vowels[i]) {
+        var output = input + "way";
+        }
+      }
+    } else if (char1 !== "a" || char1 !=="e" || char1 !=="i" || char1 !=="o" || char1 !=="u"){
     for(i=0 ; i < inputArray.length ; i++){
       if(inputArray[i]==="q" && inputArray[i + 1] === "u") {
-        var uPosition = i + 2;
-        var slice = input.slice(0, uPosition);
+        var uPosition = i + 1;
+        var slice = input.slice(0, uPosition + 1);
         var slice2 = input.slice(uPosition);
         var output = slice2 + slice + "ay";
         inputArray = [];
@@ -30,16 +39,15 @@ $(document).ready(function() {
         var slice2 = input.slice(firstVowelPosition);
         var output = slice2 + slice + "ay";
         inputArray = [];
+      } else {
+        var output = "Please enter a valid word.";
       }
     }
+  }
       alert(output);
 
 
-    for (var i = 0 ; i < vowels.length; i++) {
-      if(char1 === vowels[i]) {
-      input = input + "way";
-      }
-    }
+
 
   });
 });
