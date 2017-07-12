8336ab7a68ba5ad1dc5e1c6b19350362c413d4d2
Kimberly Lu and Trevor Gill <trevor@>
2017-07-12 11:35:36 -0700

    add js to test for word beginning with vowel and add way.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 
-// var vowels = ["a", "e", "i", "o", "u"];
-// var inputLength = input.length;
+
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+var firstChar = "";
 
 
-
-$(document).ready(function(){
+$(document).ready(function() {
   $("#latinform").submit(function(event) {
     event.preventDefault();
+
+
     var input = $("#sentenceinput").val();
-    if (input === "a") {
-      alert(input + "way");
-    } else {
-      alert("Please enter something");
+    var inputLength = input.length;
+    firstChar = input.slice(0, 1);
+
+
+    for (var i = 0 ; i < vowels.length; i++) {
+      if(firstChar === vowels[i]) {
+      input = input + "way";
+      alert(input);
     }
+    }
+
+
+    // vowels.forEach(function(vowel) {
+    //   if (input === vowel) {
+    //     alert(input + "way");
+    //   } else {
+    //     alert("Please enter something");
+    //   }
   });
 });
